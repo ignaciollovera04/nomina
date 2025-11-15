a1ffccb7d5cdd17e6428cc3991f0698e962ac1be
ignaciollovera04 <ignaciollovera04@gmail.com>
2025-11-15 15:55:14 -0500

    Limpia el proyecto y excluye carpetas bin/obj

--- Aplicacion/obj/Debug/net8.0/Aplicacion.AssemblyInfo.cs
+++ Aplicacion/obj/Debug/net8.0/Aplicacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplicacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d76b468a2ed4bb66fb481aa60cb85894ab888f93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cebeef2562667be035cc1625222a9174f6c31e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aplicacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplicacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Persistencia/obj/Debug/net8.0/Persistencia.AssemblyInfo.cs
+++ Persistencia/obj/Debug/net8.0/Persistencia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistencia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d76b468a2ed4bb66fb481aa60cb85894ab888f93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cebeef2562667be035cc1625222a9174f6c31e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistencia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistencia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
